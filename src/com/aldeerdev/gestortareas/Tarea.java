f66ff33c3bb2de68dafc35c63c1814b712693f50
AldeerDev <aldeerdev@gmail.com>
2026-02-19 21:39:20 -0300

    feat: crear modelo Tarea

---
+++ src/com/aldeerdev/gestortareas/Tarea.java
@@ -0,0 +1,35 @@
+package com.aldeerdev.gestortareas;
+
+public class Tarea {
+
+	private String descripcion;
+	private Boolean completada;
+
+	public Tarea(String descripcion) {
+		super();
+		this.descripcion = descripcion;
+		this.completada = false;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Boolean getCompletada() {
+		return completada;
+	}
+
+	public void setCompletada(Boolean completada) {
+		this.completada = completada;
+	}
+
+	@Override
+	public String toString() {
+		return (completada ? "[✓]" : "[ ]") + descripcion;
+	}
+
+}
