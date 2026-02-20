b2841734270bb86106d43e895d682cf99b5d5b97
AldeerDev <aldeerdev@gmail.com>
2026-02-19 22:11:55 -0300

    feat: agregar metodo que guarda las tareas en un archivo txt

--- src/com/aldeerdev/gestortareas/GestorTareas.java
+++ src/com/aldeerdev/gestortareas/GestorTareas.java
@@ -1,11 +1,27 @@
 package com.aldeerdev.gestortareas;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GestorTareas {
 
+	private static final String ARCHIVO = "tareas.txt";
 	private List<Tarea> tareas;
+	
+	public void guardarEnArchivo() {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(ARCHIVO));
+			for (Tarea tarea : tareas) {
+				writer.write(tarea.convertirFormatoArchivo());
+				writer.newLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error al guardar tareas: " + e.getMessage());
+		}
+	}
 
 	public GestorTareas() {
 		tareas = new ArrayList<>();
